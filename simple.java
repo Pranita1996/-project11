ea9b402bfcb4faf3e8a87dce77f02819641e7353
PRANITA <pranitaingale96@gmail.com>
2023-03-31 18:52:17 +0530

    second commit

--- simple.java
+++ simple.java
@@ -1,7 +1,8 @@
 public class simple{
     public static void main(String[]args){
         System.out.printl("hii");
-        
+        System.out.printl("welcome to my command line");
+        System.out.printl("hii okk");
 
     } 
 }
